58cb1962196f45bed0f4a223b95766dfc6a9bf7e
Beatzmoraes <nietzschea60@gmail.com>
2023-10-12 10:06:51 -0300

    testing create  invoice

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,12 +1,15 @@
+import { createTypeReferenceDirectiveResolutionCache } from 'typescript';
 import lnbits from '../lib/index';
 
 describe('tests wallet', () => {
+
+    const instance = lnbits({
+        invoiceReadKey:'3c86664d1bb6477a9b2ef1fae889fbbf',
+        adminKey:'98fd89f578ae40e8b0a5006c6c3dffed',
+        endpoint:'https://legend.lnbits.com',
+    });
+    
     test('testing wallet details', async () => {
-        const instance = lnbits({
-            invoiceReadKey:'3c86664d1bb6477a9b2ef1fae889fbbf',
-            adminKey:'98fd89f578ae40e8b0a5006c6c3dffed',
-            endpoint:'https://legend.lnbits.com',
-        });
 
         const walletDetails = await instance.wallet.walletDetails()
 
@@ -14,4 +17,15 @@ describe('tests wallet', () => {
         expect(typeof walletDetails.id).toBe('string');  
         expect(typeof walletDetails.balance).toBe('number');  
     })
+
+        test('testing create invoice',async ()=>{
+            const invoice = await instance.wallet.createInvoice({amount: 10,memo: 'description', out: false});
+            expect(typeof invoice.checking_id).toBe('string');
+            expect (typeof invoice.lnurl_response).toBe('string');
+            expect(typeof invoice.payment_hash).toBe('string');
+            expect(typeof invoice.payment_request).toBe('string');
+
+        }
+    
+    )
 })
